4df1d9f52ed47d542ca00d8b0eaed4941a51ff96
Boris Wilhelms <mail@wille-zone.de>
2020-07-12 17:56:16 +0200

    Adds install script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+DOTFILES_BACKUP=.dotfiles-backup
+
+git clone --bare git@github.com:BorisWilhelms/dotfiles.git $HOME/.dotfiles
+
+function config {
+   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+
+mkdir -p $DOTFILES_BACKUP
+config checkout
+if [ $? = 0 ]; then
+    echo "Checked out dot files.";
+else
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $DOTFILES_BACKUP/{}
+fi;
+config checkout
+config config status.showUntrackedFiles no
+config config diff.tool vimdiff
